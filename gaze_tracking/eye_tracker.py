ff68e4f9f981ddfc8d8f896db9571d64c8ff44f0
Brian Li <twinbrian236@gmail.com>
2021-07-03 17:21:27 -0700

    Update eye_tracker.py to latest

--- gaze_tracking/eye_tracker.py
+++ gaze_tracking/eye_tracker.py
@@ -1,16 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 30 19:21:18 2020
 
-@author: hp
-"""
+#***********************************************************/
+#
+# Copyright 2021 Emma Li and Brian Li. All rights reserved.
+#
+#
+#**********************************************************/
 
 import cv2
 import numpy as np
-from face_detector import get_face_detector, find_faces, draw_face
+from face_detector import get_face_detector, find_faces, draw_faces
 from face_landmarks import get_landmark_model, detect_marks, draw_marks
 import serial
 import time
+import datetime
 
 
 def eye_on_mask(mask, side, shape):
@@ -199,6 +201,46 @@ def print_eye_pos(img, left, right):
         cv2.putText(img, text, (30, 30), font,  
                    1, (0, 255, 255), 2, cv2.LINE_AA) 
 
+def process_calibration(curpos):
+    global calibration, calib_key_hit, calib_step, calib_center_pos, calib_right_pos, calib_up_pos, calib_dx, calib_dy, start_send_cmd
+
+    if not calib_key_hit:
+        return
+    calib_key_hit = False
+    if calib_step == 0:
+        calib_center_pos = curpos
+        calib_step = 1
+        print("Move eyeball to right side happy face and hit ENTER key...")
+    elif calib_step == 1:
+        calib_right_pos = curpos
+        calib_dx = calib_center_pos[0] - calib_right_pos[0]
+        calib_step = 2
+        print("Move eyeball to center happy face and hit ENTER key...")
+    elif calib_step == 2:
+        calib_center_pos = curpos
+        calib_step = 3
+        print("Move eyeball to upper happy face and hit ENTER key...")
+    elif calib_step == 3:
+        calib_up_pos = curpos
+        calib_dy = calib_up_pos[1] - calib_center_pos[1]
+        calib_res = "1," + str(calib_dx) + "," + str(calib_dy) + ";"
+        print("Sending to Arduino calibration result: " + calib_res)
+        ser.write(str.encode(calib_res))
+        print("Move onto Arduino side to calibrate follower-cam...")
+        print("After finish follower-cam calibration, look at center happy face and hit ENTER key...")
+        calib_step = 4
+    elif calib_step == 4:
+        print("Your calibration is completed!")
+        print("Switching to tracking mode...")
+        calibration = False
+        start_send_cmd = True
+        calib_step = 5
+        calib_center_pos = curpos
+    else:
+        print("Should NOT get in here!!!...")
+    
+
+
 face_model = get_face_detector()
 landmark_model = get_landmark_model()
 left = [36, 37, 38, 39, 40, 41]
@@ -212,8 +254,8 @@ cv2.namedWindow('image')
 kernel = np.ones((9, 9), np.uint8)
 
 # threshold to determine eye move and stability
-epsilon_move = 3
-epsilon_static = 0.5
+epsilon_move = 0 
+epsilon_static = 500 
 start_move = False
 prev_pos = [0, 0]  #initialize current eye position
 start_pos = [0, 0]
@@ -226,31 +268,49 @@ start_head_pos = [0, 0]
 scale_factor = 1
 
 start_send_cmd = False
+
+# Calibration mode related control vars
+calibration = False
+calib_step = 0
+calib_center_pos = [0,0]
+calib_right_pos = [0,0]
+calib_up_pos = [0,0]
+calib_dx = 0
+calib_dy = 0
+calib_key_hit = False
+
+# Reset postion to center.
+reset_key_pressed = False
+
+key = input("Enter calibration mode (y/n) ?")
+if key == "y":
+    calibration = True
+
+if calibration:
+    print("Entering calibration mode...")
+    print("Look at the center happy face and hit ENTER key...")
+else:
+    print("Entering tracking mode...")
+    start_send_cmd = True
+
 # Define the serial port to send command to arduino
-#ser = serial.Serial('/dev/cu.usbmodem14101', 115200)
+ser = serial.Serial('/dev/cu.usbmodem14101', 115200)
 
 def nothing(x):
     pass
 cv2.createTrackbar('threshold', 'image', 75, 255, nothing)
 
 while(True):
-    #time.sleep(0.5)
     ret, img = cap.read()
-    # find face box
     rects = find_faces(img, face_model)
     
     for rect in rects:
-        # find face center point to determine head movement
-        cur_head_pos = [int((rect[0] + rect[2]) // 2), int((rect[1] + rect[3]) // 2)]
-        # find 68 facial landmarks
+        
         shape = detect_marks(img, landmark_model, rect)
-        #draw_marks(img, shape)
 
         mask = np.zeros(img.shape[:2], dtype=np.uint8)
         mask, end_points_left = eye_on_mask(mask, left, shape)
-        #draw_face(img, end_points_left) 
         mask, end_points_right = eye_on_mask(mask, right, shape)
-        #draw_face(img, end_points_right)
         mask = cv2.dilate(mask, kernel, 5)
         
         eyes = cv2.bitwise_and(img, img, mask=mask)
@@ -266,56 +326,53 @@ while(True):
         eyeball_pos_right, eyeball_r = contouring(thresh[:, mid:], mid, img, end_points_right, True)
 
         valid, cur_pos = valid_eye_pos(eyeball_l, eyeball_r)
-        if valid:
-            if prev_pos[0] == 0 and prev_pos[1] == 0:
-                prev_pos = cur_pos
-                prev_head_pos = cur_head_pos
-            else:
-                #print("prev_pos: " + str(prev_pos[0]) + "," + str(prev_pos[1]))
-                #print("cur_pos: " + str(cur_pos[0]) + "," + str(cur_pos[1]))
-                delta = delta_mov(prev_pos, cur_pos)
-                #print("total delta: " + str(delta[0]) + "," + str(delta[1]))
-                #print("delta: " + str(delta[0]) + "," + str(delta[1]))
-                if abs(delta[0]) > epsilon_move or abs(delta[1]) > epsilon_move:
-                    if not start_move:
-                        print("start move")
-                        start_move = True
-                        start_pos = prev_pos
-                        start_head_pos = prev_head_pos
-                else:
-                    if start_move and abs(delta[0]) < epsilon_static and abs(delta[1]) < epsilon_static :
-                        delta = delta_mov(start_pos, cur_pos)
-                        delta_head = delta_mov(start_head_pos, cur_head_pos)
-                        print("eye movement: " + str(delta[0]) + "," + str(delta[1]))
-                        print("head movement: " + str(delta_head[0]) + "," + str(delta_head[1]))
-                        if abs(delta_head[0]) < epsilon_head_static and abs(delta_head[1]) < epsilon_head_static:
-                            # if head keep still, scale the eye movement vector
-                            delta = [delta[0]*scale_factor, delta[1]*scale_factor]
-                            print("scaled eye movement: " + str(delta[0]) + "," + str(delta[1]))
-                        #stable case
-                        # send left eyeball coordinate to serial monitor of Arduino
-                        if start_send_cmd:
-                            #ser.write(str.encode("1," + str(delta[0]) + "," + str(delta[1]) + ";"))
-                            print("sending eye movement: " + "1," + str(delta[0]) + "," + str(delta[1]) + ";")
-                        # reset eveything for next movement
-                        start_move = False
-                prev_pos = cur_pos
-                prev_head_pos = cur_head_pos
 
-        #print_eye_pos(img, eyeball_pos_left, eyeball_pos_right)
-        # for (x, y) in shape[36:48]:
-        #     cv2.circle(img, (x, y), 2, (255, 0, 0), -1)
+        if valid:
+            if calibration:
+                process_calibration(cur_pos)
+                continue
+
+            if reset_key_pressed:
+                prev_pos = calib_center_pos
+                reset_key_pressed = False
+                res = "2;"
+                print("Sending to Arduino reset signal: " + res)
+                ser.write(str.encode(res))
+
+            if prev_pos[0] == 0 and prev_pos[1] == 0:
+                print("Prev position is empty...")
+                prev_pos = cur_pos
+
+            else:
+               
+                delta = delta_mov(prev_pos, cur_pos)
+                
+                if start_send_cmd:
+                    ct = datetime.datetime.now()
+                    print("Current time: ", ct)
+                    ts = ct.timestamp()
+                    print("Timestamp: ", ts)
+                    ser.write(str.encode("0," + str(delta[0]) + "," + str(delta[1]) + ";"))
+                    print("sending eye movement: " + "0," + str(delta[0]) + "," + str(delta[1]) + ";")
+
+                prev_pos = cur_pos
+            
         
     cv2.imshow('eyes', img)
     cv2.imshow("image", thresh)
-    if cv2.waitKey(1) == ord('a'):
+    if calibration:
+        if not calib_key_hit and cv2.waitKey(1) & 0xFF== 13:
+            calib_key_hit = True
+            print("ENTER key is pressed in calibration mode.")
+        continue
+
+
+    if not start_send_cmd and cv2.waitKey(1) == ord('a'):
         print("a is pressed, start sending cmd")
         start_send_cmd = True
-    if cv2.waitKey(1) == ord('b'):
-        print("b is pressed, stop sending cmd")
-        start_send_cmd = False
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    if not reset_key_pressed and cv2.waitKey(1) == ord('r'):
+        print("r is pressed, resetting...")
+        reset_key_pressed = True
     
 cap.release()
 cv2.destroyAllWindows()
