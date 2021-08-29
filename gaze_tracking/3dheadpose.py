617e4cd81e5865a898d51d56515b33266f027dbf
Brian Li <86897114+twinbrian@users.noreply.github.com>
2021-08-28 17:45:31 -0700

    Add files via upload

--- gaze_tracking/3dheadpose.py
+++ gaze_tracking/3dheadpose.py
@@ -1,168 +1,85 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 31 03:00:36 2020
-
-@author: hp
-"""
-
 import cv2
 import numpy as np
 import math
+#import serial
 from face_detector import get_face_detector, find_faces
 from face_landmarks import get_landmark_model, detect_marks
 
-def get_2d_points(img, rotation_vector, translation_vector, camera_matrix, val):
-    """Return the 3D points present as 2D for making annotation box"""
-    point_3d = []
-    dist_coeffs = np.zeros((4,1))
-    rear_size = val[0]
-    rear_depth = val[1]
-    point_3d.append((-rear_size, -rear_size, rear_depth))
-    point_3d.append((-rear_size, rear_size, rear_depth))
-    point_3d.append((rear_size, rear_size, rear_depth))
-    point_3d.append((rear_size, -rear_size, rear_depth))
-    point_3d.append((-rear_size, -rear_size, rear_depth))
-    
-    front_size = val[2]
-    front_depth = val[3]
-    point_3d.append((-front_size, -front_size, front_depth))
-    point_3d.append((-front_size, front_size, front_depth))
-    point_3d.append((front_size, front_size, front_depth))
-    point_3d.append((front_size, -front_size, front_depth))
-    point_3d.append((-front_size, -front_size, front_depth))
-    point_3d = np.array(point_3d, dtype=np.float).reshape(-1, 3)
-    
-    # Map to 2d img points
-    (point_2d, _) = cv2.projectPoints(point_3d,
-                                      rotation_vector,
-                                      translation_vector,
-                                      camera_matrix,
-                                      dist_coeffs)
-    point_2d = np.int32(point_2d.reshape(-1, 2))
-    return point_2d
 
-def draw_annotation_box(img, rotation_vector, translation_vector, camera_matrix,
-                        rear_size=300, rear_depth=0, front_size=500, front_depth=400,
-                        color=(255, 255, 0), line_width=2):
+def isRotationMatrix(R) :
     """
-    Draw a 3D anotation box on the face for head pose estimation
-
+    Check if a matrix is rotation matrix by verify R'* R = I'
     Parameters
-    ----------
-    img : np.unit8
-        Original Image.
-    rotation_vector : Array of float64
-        Rotation Vector obtained from cv2.solvePnP
-    translation_vector : Array of float64
-        Translation Vector obtained from cv2.solvePnP
-    camera_matrix : Array of float64
-        The camera matrix
-    rear_size : int, optional
-        Size of rear box. The default is 300.
-    rear_depth : int, optional
-        The default is 0.
-    front_size : int, optional
-        Size of front box. The default is 500.
-    front_depth : int, optional
-        Front depth. The default is 400.
-    color : tuple, optional
-        The color with which to draw annotation box. The default is (255, 255, 0).
-    line_width : int, optional
-        line width of lines drawn. The default is 2.
-
+    -----------
+    R: 3x3 matrix
     Returns
-    -------
-    None.
-
+    --------
+    true if R is a rotation matrix
     """
-    
-    rear_size = 1
-    rear_depth = 0
-    front_size = img.shape[1]
-    front_depth = front_size*2
-    val = [rear_size, rear_depth, front_size, front_depth]
-    point_2d = get_2d_points(img, rotation_vector, translation_vector, camera_matrix, val)
-    # # Draw all the lines
-    cv2.polylines(img, [point_2d], True, color, line_width, cv2.LINE_AA)
-    cv2.line(img, tuple(point_2d[1]), tuple(
-        point_2d[6]), color, line_width, cv2.LINE_AA)
-    cv2.line(img, tuple(point_2d[2]), tuple(
-        point_2d[7]), color, line_width, cv2.LINE_AA)
-    cv2.line(img, tuple(point_2d[3]), tuple(
-        point_2d[8]), color, line_width, cv2.LINE_AA)
-    
-    
-def head_pose_points(img, rotation_vector, translation_vector, camera_matrix):
-    """
-    Get the points to estimate head pose sideways    
-
-    Parameters
-    ----------
-    img : np.unit8
-        Original Image.
-    rotation_vector : Array of float64
-        Rotation Vector obtained from cv2.solvePnP
-    translation_vector : Array of float64
-        Translation Vector obtained from cv2.solvePnP
-    camera_matrix : Array of float64
-        The camera matrix
-
-    Returns
-    -------
-    (x, y) : tuple
-        Coordinates of line to estimate head pose
-
-    """
-    rear_size = 1
-    rear_depth = 0
-    front_size = img.shape[1]
-    front_depth = front_size*2
-    val = [rear_size, rear_depth, front_size, front_depth]
-    point_2d = get_2d_points(img, rotation_vector, translation_vector, camera_matrix, val)
-    y = (point_2d[5] + point_2d[8])//2
-    x = point_2d[2]
-    
-    return (x, y)
-
-##def isRotationMatrix(R) :
-    ##Rt = np.transpose(R)
-    ##shouldBeIdentity = np.dot(Rt, R)
-    ##I = np.identity(3, dtype = R.dtype)
-    ##n = np.linalg.norm(I - shouldBeIdentity)
-    ##return n < 1e-6
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype = R.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+ 
 
 def rotationMatrixToEulerAngles(R) :
-    ##if not isRotationMatrix(R) :
-        ##return np.array([0,0,0])
-    sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+    """
+    Calculates rotation matrix to euler angles based on our derived formula
+    """
+
+    #sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+    sy = math.sqrt(R[2, 1] * R[2, 1] + R[2, 2] * R[2, 2])
     singular = sy < 1e-6
-    if not singular :
-        print("xCoordVals: {} {}".format(R[2,1],R[2,2]))
-        x = math.atan2(R[2,1] , R[2,2])
-        print("yCoordVals: {} {}".format(-R[2,0],sy))
-        y = math.atan2(-R[2,0], sy)
-        print("zCoordVals: {} {}".format(R[1,0],R[0,0]))
-        z = math.atan2(R[1,0], R[0,0])
+    if  not singular :
+        x = math.degrees(math.atan2(R[2,1] , R[2,2]))
+        y = math.degrees(math.atan2(-R[2,0], sy))
+        z = math.degrees(math.atan2(R[1,0], R[0,0]))
     else :
-        print("xCoordVals: {} {}".format(-R[1,2],R[1,1]))
-        x = math.atan2(-R[1,2], R[1,1])
-        print("yCoordVals: {} {}".format(-R[2,0],sy))
-        y = math.atan2(-R[2,0], sy)
+        print("Encounter singular rotation matrix")
+        # TODO: this is suspicous, since sy is 0, then y will be undefined
+        x = math.degrees(math.atan2(-R[1,2], R[1,1]))
+        y = math.degrees(math.atan2(-R[2,0], sy))
         z = 0
     return np.array([x, y, z])
 
-##def rotationMatrixToEulerAngles(R) :
-    ##print("xCoordVals: {} {}".format(-R[1,2],R[2,2]))
-    ##xCoord = math.atan2(-R[1,2],R[2,2])
-    ##print("yCoordVals: {}".format(R[0,2]))
-    ##if R[0,2] > 1 or R[0,2] < -1:
-        ##yCoord = 0
-    ##else:
-        ##yCoord = math.asin(R[0,2])
-    ##print("zCoordVals: {} {}".format(-R[0,1],R[0,0]))
-    ##zCoord = math.atan2(-R[0,1], R[0,0])
-    ##return np.array([xCoord, yCoord, zCoord])
+def eulerAngleFromPnP(rotation_vec, translation_vec):
+    # Calculate euler angle
+    rotation_mat, _ = cv2.Rodrigues(rotation_vec)
+    pose_mat = cv2.hconcat((rotation_mat, translation_vec))
+    _, _, _, _, _, _, euler_angles = cv2.decomposeProjectionMatrix(pose_mat)
+    return (rotation_mat, euler_angles)
 
+
+def eulerAngleFromRotTrans(rotation_mat, translation_vec):
+    # Calculate euler angle
+    pose_mat = cv2.hconcat((rotation_mat, translation_vec))
+    _, _, _, _, _, _, euler_angles = cv2.decomposeProjectionMatrix(pose_mat)
+    return (rotation_mat, euler_angles)
+       
+
+def relativeRotation(R_ca, R_cb) :
+    """
+    Calculate the relative rotation of object B in the frame of object A.
+    Parameters
+    -----------
+    R_ca: 3x3 rotation matrix of object A in the camera frame (or a transformation from the coordinate frame of
+           A to the camera coordinate frame)
+    R_cb: 3x3 rotation matrix of object B in the camera frame (or a transformation from the coordinate frame of
+           B to the camera coordinate frame)
+    Returns
+    --------
+    R_ab: 3x3 rotation matrix of object B in frame of object A.
+    R_ab = R_ac * R_cb = inverse(R_ca) * R_cb = R_ca' * R_cb
+    """
+    #print("Comparing before {} and after {}".format(R_ca, R_cb))
+    relativeMove = np.linalg.norm(R_ca - R_cb)
+    R_ac = np.transpose(R_ca)
+    return np.dot(R_ac, R_cb)
+
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging
+import tensorflow as tf
+tf.get_logger().setLevel('ERROR')           # Suppress TensorFlow logging (2)  
 
 face_model = get_face_detector()
 landmark_model = get_landmark_model()
@@ -170,6 +87,8 @@ cap = cv2.VideoCapture(0)
 ret, img = cap.read()
 size = img.shape
 font = cv2.FONT_HERSHEY_SIMPLEX 
+#ser = serial.Serial('/dev/cu.usbmodem14101', 115200)
+
 # 3D model points.
 model_points = np.array([
                             (0.0, 0.0, 0.0),             # Nose tip
@@ -188,9 +107,13 @@ camera_matrix = np.array(
                          [0, focal_length, center[1]],
                          [0, 0, 1]], dtype = "double"
                          )
-prev = np.zeros((3,3))
-cur = np.zeros((3,3))
-firstTime = True
+
+prev_rotation = np.zeros((3, 3))
+prev_translation = np.zeros((3, 1))
+initialized = False
+
+start_pos = False
+end_pos = False
 while True:
     ret, img = cap.read()
     if ret == True:
@@ -208,25 +131,10 @@ while True:
                                 ], dtype="double")
             dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
             (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_UPNP)
-            if firstTime == True:
-                prev = rotation_vector
-                firstTime = False
-                continue
-            cur = rotation_vector
-            relative_rotation = cur.dot(prev.transpose())
-            print("previous rotation matrix: {}".format(prev))
-            # print("current rotation matrix:" + cur)
-            # print("relative rotation matrix:" + relative_rotation)
-            #Use the formula for euler angle, remember we dont need theta_z
-            (theta_x, theta_y, theta_z) = rotationMatrixToEulerAngles(relative_rotation)
-            # Feed these into the arduino for the follower cam to rotate
-            prev = rotation_vector
-            #We have finished the rotation for the system by here
-            print("3D rotation euler angles are ({}, {}, {})".format(theta_x, theta_y, theta_z))
+
             
             # Project a 3D point (0, 0, 1000.0) onto the image plane.
-            # We use this to draw a line sticking out of the nose
-            
+            # We use this to draw a line sticking out of the nose            
             (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
             
             for p in image_points:
@@ -235,43 +143,52 @@ while True:
             
             p1 = ( int(image_points[0][0]), int(image_points[0][1]))
             p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
-            x1, x2 = head_pose_points(img, rotation_vector, translation_vector, camera_matrix)
 
             cv2.line(img, p1, p2, (0, 255, 255), 2)
-            cv2.line(img, tuple(x1), tuple(x2), (255, 255, 0), 2)
-            # for (x, y) in marks:
-            #     cv2.circle(img, (x, y), 4, (255, 255, 0), -1)
-            # cv2.putText(img, str(p1), p1, font, 1, (0, 255, 255), 1)
-            try:
-                m = (p2[1] - p1[1])/(p2[0] - p1[0])
-                ang1 = int(math.degrees(math.atan(m)))
-            except:
-                ang1 = 90
-                
-            try:
-                m = (x2[1] - x1[1])/(x2[0] - x1[0])
-                ang2 = int(math.degrees(math.atan(-1/m)))
-            except:
-                ang2 = 90
-                
-                # print('div by zero error')
-            if ang1 >= 48:
-                print('Head down')
-                cv2.putText(img, 'Head down', (30, 30), font, 2, (255, 255, 128), 3)
-            elif ang1 <= -48:
-                print('Head up')
-                cv2.putText(img, 'Head up', (30, 30), font, 2, (255, 255, 128), 3)
-             
-            if ang2 >= 48:
-                print('Head right')
-                cv2.putText(img, 'Head right', (90, 30), font, 2, (255, 255, 128), 3)
-            elif ang2 <= -48:
-                print('Head left')
-                cv2.putText(img, 'Head left', (90, 30), font, 2, (255, 255, 128), 3)
-            
-            cv2.putText(img, str(ang1), tuple(p1), font, 2, (128, 255, 255), 3)
-            cv2.putText(img, str(ang2), tuple(x1), font, 2, (255, 255, 128), 3)
+            # Calculate euler angle
+            (rmat, euler_angles) = eulerAngleFromPnP(rotation_vector, translation_vector)
+            # print("Translation vector: {}".format(translation_vector))
+            # print("Rotation matrix: {}".format(rmat))
+            # print("Euler angles: {}".format(euler_angles))
+            # calculate relative rotation and translation relative to previous frame
+            if not isRotationMatrix(rmat):
+                print("ignore an invalid rotation matrix {} from solvePnP after cv2.Rodrigues".format(rmat))
+                continue
+            # if not initialized:
+            if start_pos:
+                prev_rotation = rmat
+                prev_translation = translation_vector
+                initialized = True
+                start_pos = False
+                print("Start position rotation matrix {}".format(rmat))
+                continue
+            #else:
+            if end_pos:
+                print("End position rotation matrix {}".format(rmat))
+                relative_rotation = relativeRotation(prev_rotation, rmat)
+                relative_translation = np.subtract(translation_vector, prev_translation)
+                #print("Relative translation_vec: {}".format(relative_translation))
+                if not isRotationMatrix(relative_rotation):
+                    print("Ignore invalid relative rotation matrix in the frame of previous pos {}".format(relative_rotation))
+                    continue
+                my_relative_euler = rotationMatrixToEulerAngles(relative_rotation)
+                #[theta_x, theta_y, theta_z] = rotationMatrixToEulerAngles(relative_rotation)
+                (_, relative_euler) = eulerAngleFromRotTrans(relative_rotation, relative_translation)
+                #ser.write(str.encode("0," + str(theta_y) + "," + str(theta_x) + ";"))
+                print("Relative rotation euler angles: {}".format(relative_euler))
+                #print("sending eye movement: " + "0," + str(theta_y) + "," + str(theta_x) + ";")
+                prev_rotation = rmat
+                prev_translation = translation_vector
+                end_pos = False
+
         cv2.imshow('img', img)
+        if cv2.waitKey(1) == ord('a'):
+            print("a is pressed, start sending cmd")
+            start_pos = True
+        if cv2.waitKey(1) == ord('b'):
+            print("b is pressed, stop sending cmd")
+            end_pos = True
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
